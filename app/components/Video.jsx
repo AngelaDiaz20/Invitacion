cdafb96075b82b418c1eaba031926d857e421c7f
Angela Diaz <angeldi846@gmail.com>
2024-06-14 09:29:37 -0500

    Ajuste video

--- app/components/Video.jsx
+++ app/components/Video.jsx
@@ -1,37 +1,89 @@
 "use client";
-import React, { useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { MdOutlineReplay } from "react-icons/md";
+import { IoMdVolumeMute } from "react-icons/io";
+import { IoVolumeMute } from "react-icons/io5";
 
-const Video = ({ src }) => {
+const AutoPlayVideo = () => {
   const videoRef = useRef(null);
-  const [showRepeatButton, setShowRepeatButton] = useState(false);
+  const [isMuted, setIsMuted] = useState(true);
+  const [isEnded, setIsEnded] = useState(false);
 
-  const handleVideoEnd = () => {
-    setShowRepeatButton(true);
+  useEffect(() => {
+    const video = videoRef.current;
+
+    const playVideo = () => {
+      if (video && video.paused) {
+        video.play().catch(error => {
+          console.error('Error trying to play video:', error);
+        });
+      }
+    };
+
+    // Try to play the video on mount
+    playVideo();
+
+    // Add event listener to ensure video plays when in view
+    const handleVisibilityChange = () => {
+      if (document.visibilityState === 'visible') {
+        playVideo();
+      }
+    };
+
+    document.addEventListener('visibilitychange', handleVisibilityChange);
+
+    return () => {
+      document.removeEventListener('visibilitychange', handleVisibilityChange);
+    };
+  }, []);
+
+  const handleMuteToggle = () => {
+    const video = videoRef.current;
+    video.muted = !video.muted;
+    setIsMuted(video.muted);
   };
 
-  const handleRepeat = () => {
-    videoRef.current.currentTime = 0;
-    videoRef.current.play();
-    setShowRepeatButton(false);
+  const handleVideoEnded = () => {
+    setIsEnded(true);
+    const video = videoRef.current;
+    video.currentTime = video.duration;
+  };
+
+  const handleRestart = () => {
+    const video = videoRef.current;
+    video.currentTime = 0;
+    setIsEnded(false);
+    video.play();
   };
 
   return (
-    <div className="video-container">
+    <div className="video-container relative">
       <video
         ref={videoRef}
-        src={src}
+        src="/assets/video/INICIO.mp4"
         autoPlay
-        controls={false}
-        onEnded={handleVideoEnd}
+        muted={isMuted}
+        playsInline
+        loop={false}
+        onEnded={handleVideoEnded}
+        className="your-custom-class w-full"
       />
-      {showRepeatButton && (
-        <button onClick={handleRepeat} className="repeat-button">
-          <MdOutlineReplay className='text-white'/>
+      <button
+        onClick={handleMuteToggle}
+        className="mute-button absolute top-4 left-4 p-2 rounded bg-[#7A9285] hover:bg-[#202723] m-auto"
+      >
+         {isMuted ? <IoVolumeMute size={24} /> : <IoMdVolumeMute size={24} />}
+      </button>
+      {isEnded && (
+        <button
+          onClick={handleRestart}
+          className="restart-button absolute top-4 right-4 bg-[#7A9285] hover:bg-[#202723] p-2 rounded text-white text-[24px] font-bold "
+        >
+          <MdOutlineReplay />
         </button>
       )}
     </div>
   );
 };
 
-export default Video;
+export default AutoPlayVideo;
